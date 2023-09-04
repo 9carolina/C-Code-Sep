a0871b969045e08723bdb09f0230a484f463d662
9carolina <141955867+9carolina@users.noreply.github.com>
2023-09-03 20:07:56 -0600

    Add files via upload

---
+++ Sin Nombre2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a=9, b=3, c=2, sumaValores, moduloValores;
+	float divisionValores;
+	
+	if(a+b==12)
+	{
+		printf("\n  *\n ***\n*****\n  |\n\n");
+		
+		if(c/a<=1)
+		{
+			printf("\n    *\n   ***\n  *****\n *******\n*********\n    |\n\n");
+		}		
+	}
+	
+	else(b%c==1);
+		{
+			printf("\n      *\n     ***\n    *****\n   *******\n  *********\n ***********\n*************\n       |\n\n");
+		}
+	
+	return 0;
+}
+
