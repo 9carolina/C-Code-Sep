ab0c2bcc7e409dbef99aee852d971f314db637bd
9carolina <141955867+9carolina@users.noreply.github.com>
2023-09-12 12:37:17 -0600

    Add files via upload

---
+++ ejercicio 1.c
@@ -0,0 +1,31 @@
+/*Genera un número específico de números aleatorios y calcularás su suma. 
+Puedes ajustar la variable numNumerosAleatorios para cambiar la cantidad de números aleatorios que deseas sumar
+*/
+ #include<stdio.h>
+ #include<stdlib.h>
+ #include<time.h>
+ 
+ int main ()
+ {
+	
+	int num;
+	int numeroAleatorio =rand()%100;
+	srand(time(NULL));
+	
+	printf("Cantidad de numeros que deseas sumar: \n");
+	scanf("%d", &num);
+	
+	int d=0;
+	int suma=0;
+	while (d<num)
+	{
+		numeroAleatorio =rand()%51;
+		printf("Numero aleatorio: %d\n", numeroAleatorio);
+		suma=suma+numeroAleatorio;
+		d++;
+	}
+ 	
+	 printf("La suma de los numeros aleatorios es: %d\n", suma);
+	  	
+ 	return 0;
+ }
---
+++ ejercicio 3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	int a=3;
+	int num;
+	srand (time(NULL));
+	int r;
+	int contador =1;
+
+	printf("Cantidad de lanzamientos: ");	
+	scanf("%d", &num);
+	
+	while (contador <= num)
+	{
+		r = rand()*1;
+		r = r%7;
+		printf("Numero %d: %d\n", contador, r);
+		contador++;
+		if (r==3)
+		{
+		
+		}
+	}
+	printf ("Aparece %d, %d veces", a);
+	
+	return 0;
+}
